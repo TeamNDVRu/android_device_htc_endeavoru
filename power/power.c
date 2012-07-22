6e4b34d8bbc0c7117a13eb635cae85daaeac3bb6
MDJ <melodydeejay@gmail.com>
2012-07-22 12:44:03 +0800

    640MHz enough for screen off

--- power/power.c
+++ power/power.c
@@ -54,7 +54,7 @@ static void endeavoru_power_init(struct power_module *module)
 {
     /*
      * cpufreq interactive governor: timer 20ms, min sample 100ms,
-     * hispeed 1000MHz at load 40%
+     * hispeed 700MHz at load 40%
      */
 
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate",
@@ -77,7 +77,7 @@ static void endeavoru_power_set_interactive(struct power_module *module, int on)
      */
 
     sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                on ? "1500000" : "1000000");
+                on ? "1500000" : "640000");
 
     sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/input_boost",
                 on ? "1" : "0");
